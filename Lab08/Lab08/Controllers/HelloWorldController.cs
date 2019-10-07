aaa5da8f46d1bbb6283788c11b5c3e70e897fd43
Josimar Tantahuilca <jtantahuilca@truenorth.pr>
2019-10-07 18:42:47 -0500

    Lab08

---
+++ Lab08/Lab08/Controllers/HelloWorldController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab08.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        //public string Index()
+        //{
+        //    return "This is my <b>default</b> action...";
+        //}
+
+        // URL: /HelloWorld/Welcome?name=Mike&numtimes=4
+        public ActionResult Welcome(string name, int  numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+
+        //public string Welcome(string name, int ID = 1)
+        //{
+        //    return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+        //}
+    }
+}
